f7e5801de8457ee87a1cfa747bccd1eb31293495
Manolo Betancur <olonammanolo@gmail.com>
2021-02-01 10:21:17 -0500

    Tabla del 3

--- tabla.py
+++ tabla.py
@@ -1,7 +1,7 @@
 def main():
-	print("primero la tabla del 2")
+	print("Ahora la tabla del 3")
 	for e in range(11):
-		x=2*e
-		print("Operacion 2 x {} = {}".format(e,x))
+		x=3*e
+		print("Operacion 3 x {} = {}".format(e,x))
 if __name__ =="__main__":
 	main()
